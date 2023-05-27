3c348c3722019fa7eb6b8226b69ad701f11a0a5f
AkshayaPatil5 <akshayapatil1987@gmail.com>
2023-05-27 16:32:03 +0530

    qsa6task

---
+++ task6b.js
@@ -0,0 +1,7 @@
+
+var odd=document.querySelectorAll("li:nth-child(odd)");
+for(var i=0; i<odd.length;i++){
+    odd[i].style.backgroundColor="green";
+}
+var secItem=document.querySelectorAll(".list-group-item");
+secItem[1].style.color="green";
