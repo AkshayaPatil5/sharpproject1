dab387740b4a021418e34ba8f2b1b9b57d0e0daa
AkshayaPatil5 <akshayapatil1987@gmail.com>
2023-05-27 16:10:38 +0530

    QStask6

---
+++ task6.js
@@ -0,0 +1,5 @@
+var secItem = document.querySelector(".list-group-item:nth-child(2)");
+secItem.style.background="green";
+
+var thirdItem = document.querySelector(".list-group-item:nth-child(3)");
+thirdItem.style.display="none";
