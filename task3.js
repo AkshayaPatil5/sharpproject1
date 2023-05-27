a38e4e14408f63fdde26063cd7d464eb0641d2a4
AkshayaPatil5 <akshayapatil1987@gmail>
2023-05-27 12:17:39 +0530

    thirdtask

---
+++ task3.js
@@ -0,0 +1,18 @@
+//console.dir(document);
+
+
+//console.log(document.URL);
+//console.log(document.title);
+//console.log(document.all);
+
+//getelementbyid
+//console.log(document.getElementById("header-title)"));
+var headerTitle=document.getElementById("main-header");
+//console.log(headerTitle);
+//headerTitle.textContent="Hello";
+//headerTitle.innertext="Goodbye";
+
+headerTitle.style.borderBottom="solid 3px #000";
+var Title=document.getElementById("main-title");
+Title.style.fontWeight="900"
+Title.style.color="green"
