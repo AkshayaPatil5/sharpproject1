5a2abe3c99d0c73f6ab2de4d6274273384192303
AkshayaPatil5 <akshayapatil1987@gmail.com>
2023-05-27 14:04:37 +0530

    4thtask

--- task3.js
+++ task3.js
@@ -16,3 +16,13 @@ headerTitle.style.borderBottom="solid 3px #000";
 var Title=document.getElementById("main-title");
 Title.style.fontWeight="900"
 Title.style.color="green"
+
+
+
+var items=document.getElementsByClassName("list-group-item");
+items[2].style.background="green";
+
+for(var i=0; i<items.length;i++){
+    items[i].style.fontWeight="bold";
+}
+
